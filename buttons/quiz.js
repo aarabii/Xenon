b66323b1045d0e83b4c17b8a9d699d5493235302
w3dg <51162936+w3dg@users.noreply.github.com>
2022-09-04 19:26:51 +0530

    Adds Database for Quiz

--- buttons/quiz.js
+++ buttons/quiz.js
@@ -1,10 +1,29 @@
-const { EmbedBuilder, MessageEmbed, ApplicationCommandType } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+const mongoose = require("mongoose");
+
+const QUIZ_MONGODB_URI = process.env.QUIZ_MONGODB_URI || "mongodb://localhost:27017/quiz";
+
+const pointsSchema = new mongoose.Schema({
+  id: String,
+  points: Number,
+});
+
+const PointsModel = mongoose.model("Points", pointsSchema);
+
+async function connectToDB() {
+  await mongoose.connect(QUIZ_MONGODB_URI);
+}
+
+async function saveToDB(playerPoint) {
+  await playerPoint.save();
+}
+
+connectToDB();
 
 module.exports = {
   name: "quiz",
   run: async (client, interaction) => {
     let embed;
-
     // if the same person responds, not anyone else in the server
     if (interaction.user.id === interaction.message.interaction.user.id) {
       if (interaction.customId === "TRUE") {
@@ -14,6 +33,18 @@ module.exports = {
           .setColor("#ffffff")
           .setTimestamp()
           .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL() });
+
+        const userPointsForUser = await PointsModel.findOne({ id: interaction.user.id }).exec();
+
+        if (!userPointsForUser) {
+          // if user not present already
+          const playerPoint = new PointsModel({ id: interaction.user.id, points: 5 });
+          await saveToDB(playerPoint);
+        } else {
+          // update the one found
+          const points = userPointsForUser.points;
+          await PointsModel.findOneAndUpdate({ id: interaction.user.id }, { $set: { points: points + 5 } });
+        }
       } else {
         embed = new EmbedBuilder()
           .setTitle("Quiz Answer")
