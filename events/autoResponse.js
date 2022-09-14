b9facd7606188d32d7488ea593e6f950cd170f13
ZZCWC <85448514+losier@users.noreply.github.com>
2022-09-14 21:16:31 +0530

    updated autoResponse

--- events/autoResponse.js
+++ events/autoResponse.js
@@ -42,48 +42,48 @@ client.on('messageCreate', async (msg) => {
         })
     }
 
-    if(msg.content.toLowerCase().includes('hmm')) {
+    // if(msg.content.toLowerCase().includes('hmm')) {
 
-        const replyArr = [
-            "Dekh bahi...\nBaat karni hai toh kar ye **Bhains** ki trah hmm hmm mat kar... :|",
-            "Bhains ki trah hmm hmm mat kar... :|",
-            "Har bar sirf hmm yo lo gillet se shave kro aur jo marzi bolo... :)"
-        ]
+    //     const replyArr = [
+    //         "Dekh bahi...\nBaat karni hai toh kar ye **Bhains** ki trah hmm hmm mat kar... :|",
+    //         "Bhains ki trah hmm hmm mat kar... :|",
+    //         "Har bar sirf hmm yo lo gillet se shave kro aur jo marzi bolo... :)"
+    //     ]
 
-        const reply = replyArr[Math.floor(Math.random() * replyArr.length)];
-        msg.reply({
-            content: `${reply}`,
-        })
-    }
+    //     const reply = replyArr[Math.floor(Math.random() * replyArr.length)];
+    //     msg.reply({
+    //         content: `${reply}`,
+    //     })
+    // }
 
-    if(msg.content.toLowerCase().includes('kya hua')) {
-        msg.reply({
-            content: `Gand faat gai, siyoge... :|`
-        })
-    }
+    // if(msg.content.toLowerCase().includes('kya hua')) {
+    //     msg.reply({
+    //         content: `Gand faat gai, siyoge... :|`
+    //     })
+    // }
 
-    if(msg.content.length > 150) {
-        msg.reply({
-            content: 'Kripiya yaha gayan na batein...'
-        })
-    }
+    // if(msg.content.length > 150) {
+    //     msg.reply({
+    //         content: 'Kripiya yaha gayan na batein...'
+    //     })
+    // }
 
-    if(msg.content.includes('<@1010412624984408156>')) {
-        msg.reply({
-            content: 'Kya hogya faltu ke ping kyu kr rha bahi :)'
-        })
-    }
+    // if(msg.content.includes('<@1010412624984408156>')) {
+    //     msg.reply({
+    //         content: 'Kya hogya faltu ke ping kyu kr rha bahi :)'
+    //     })
+    // }
 
-    if(msg.content.toLowerCase().includes('simp')){
-        msg.reply({
-            content: 'Simping chal rahi hai ky iddhar 😏😏'
-        })
-    }
+    // if(msg.content.toLowerCase().includes('simp')){
+    //     msg.reply({
+    //         content: 'Simping chal rahi hai ky iddhar 😏😏'
+    //     })
+    // }
 
-    if(msg.content.toLowerCase().includes('bahi')){
-        msg.reply({
-            content: 'Haa bahi bol...'
-        })
-    }
+    // if(msg.content.toLowerCase().includes('bahi')){
+    //     msg.reply({
+    //         content: 'Haa bahi bol...'
+    //     })
+    // }
 
 })
